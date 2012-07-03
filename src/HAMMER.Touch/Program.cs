ca5f4df5aa056b8708f95fbb86eed700f52a65b0
Paul Jenkins <paul@jenkinsfamily.com.au>
2012-07-03 18:49:56 +1000

    You can't..

---
+++ src/HAMMER.Touch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HAMMER.Touch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Can't touch this");
+        }
+    }
+}
